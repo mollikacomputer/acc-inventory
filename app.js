afb4810c067a2b20bd0cfcd8acd147751cda2293
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-13 04:54:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('App is running for ')
+});
+
+app.listen(port, () =>{
+    console.log(`Listening to port ${port}`);
+});
