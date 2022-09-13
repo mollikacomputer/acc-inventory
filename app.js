d13b03cb3719fec4ba16abf4bb432b312ff9f9da
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-13 07:47:14 +0600

    .env file at the end of line no need semiclone sign ;

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +9,5 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
+// const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+// DBConnect();
+
+// DATABASE CONNECTION DETAILS
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+  console.log(`Database connection is successful`);
+})
 
 // server
 const port = process.env.PORT || 8080;
+// user id acc-inventory
+// password 5uoVU4pbl9B76VWK
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
